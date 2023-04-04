c365ca1616c8b55befe5d69f61285253cc0584e9
Jihyun Lee <zzh23@naver.com>
2023-04-04 13:42:31 +0900

    Initial Commit

---
+++ Shared/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  Shared
+//
+//  Created by 이지현 on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
